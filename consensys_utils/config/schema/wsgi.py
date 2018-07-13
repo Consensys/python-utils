88da9a73c9e459a8ebb0ba1e9f5194de89155410
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-07-14 00:10:00 +0200

    feat(config): implement WSGI config schema

---
+++ consensys_utils/config/schema/wsgi.py
@@ -0,0 +1,59 @@
+"""
+    consensys_utils.config.schema.wsgi
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    WSGI middlewares configuration schema
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+from ...utils import import_optional_module
+
+cfg_loader = import_optional_module('cfg_loader')
+fields = import_optional_module('marshmallow.fields')
+
+
+class RequestIDConfigSchema(cfg_loader.ConfigSchema):
+    """Request ID Middleware configuration
+
+    Describes and validates against
+
+    .. list-table::
+        :widths: 30 50 20
+        :header-rows: 1
+
+        * - Key
+          - Comment
+          - Default value
+
+        * - ``REQUEST_ID_HEADER``
+          - Required header where to load/inject correlation ID
+          - 'X-Request-ID'
+    """
+
+    # Request header indicating request ID (used as correlation ID for logs)
+    REQUEST_ID_HEADER = fields.Str(missing='X-Request-ID')
+
+
+class WSGIConfigSchema(cfg_loader.ConfigSchema):
+    """Configuration relative to wsgi middlewares
+
+    Describes and validates against
+
+    .. list-table::
+        :widths: 30 50 20
+        :header-rows: 1
+
+        * - Key
+          - Comment
+          - Default value
+
+        * - ``request_id``
+          - Request ID configuration in :class:`RequestIDConfigSchema`
+          - :class:`RequestIDConfigSchema` default
+    """
+
+    # Request id middleware section
+    request_id = fields.Nested(RequestIDConfigSchema,
+                               RequestIDConfigSchema().load({}))

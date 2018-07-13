82a284003d692bbec7f5c24ce9455af51f7e73b3
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-07-13 22:22:31 +0200

    test(utils): implement tests for import_optional_module

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+"""
+    tests.test_utils
+    ~~~~~~~~~~~~~~~~
+
+    Test utility functions
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import pytest
+
+from consensys_utils.utils import import_optional_module
+
+
+def test_import_optional_module_missing():
+    with pytest.raises(ModuleNotFoundError) as e:
+        import_optional_module('missing_module')
+    assert str(e.value) == "To use '{}' make sure to have 'missing_module' installed".format(__name__)

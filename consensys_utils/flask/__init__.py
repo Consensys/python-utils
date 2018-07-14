71daa9cb6cb55645565d06e66bad03cb7e99682d
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-07-15 00:09:39 +0200

    fix(flask): clean import organization

---
+++ consensys_utils/flask/__init__.py
@@ -0,0 +1,21 @@
+"""
+    consensys_utils.flask
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Resources for Flask applications
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+from .app import Flask
+from .extensions import initialize_extensions
+from .hooks import set_hooks
+from .wsgi import apply_middlewares
+
+__all__ = [
+    'Flask',
+    'apply_middlewares',
+    'initialize_extensions',
+    'set_hooks',
+]

5550e073d5d0dc120d262b290e9b2bc7be35dffc
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-07-23 20:56:10 +0200

    feat(gunicorn.logging): implement logging facilities for gunicorn

--- consensys_utils/logging.py
+++ consensys_utils/logging.py
@@ -24,17 +24,22 @@ class IDFilter(logging.Filter):
         return True
 
 
-def create_logger(config):
+def create_logger(config, default_config=None):
     """Create logger
 
     :param config: Logging configuration
     :type config: dict
+    :param default_config: Default logging configuration
+    :type default_config: dict
     """
 
-    if 'LOGGING_CONFIG_PATH' in config:  # pragma: no branch
+    final_config = (default_config or {}).copy()
+    final_config.update(config)
+
+    if 'LOGGING_CONFIG_PATH' in final_config:  # pragma: no branch
         # Assumes we are trying to load configuration from a .yml file
         from cfg_loader.utils import parse_yaml_file
-        config = parse_yaml_file(config['LOGGING_CONFIG_PATH'])
+        final_config.update(parse_yaml_file(final_config['LOGGING_CONFIG_PATH']))
 
-    if config:  # pragma: no branch
-        dictConfig(config)
+    if final_config:  # pragma: no branch
+        dictConfig(final_config)

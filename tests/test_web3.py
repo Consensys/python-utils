70ecbf48f9a3de25ca691f058bc7dffc60130397
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-07-31 16:44:37 +0200

    feat(web3): implement web3 resources

---
+++ tests/test_web3.py
@@ -0,0 +1,9 @@
+"""
+    tests.flask.test_web3
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Test Web3 resources
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see LICENSE for more details.
+"""
